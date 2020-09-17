1723bf8f6f5850e6982c6191dc97f1c380451287
ccoll86 <70548702+ccoll86@users.noreply.github.com>
2020-09-17 16:15:00 -0500

    Update importurl.py

--- importurl.py
+++ importurl.py
@@ -5,31 +5,29 @@ file = open("aws.log", "r")
 
 #start looking for total log requests
 def main1():
-total_log_requests=int()
-    lines = file.readlines()
-    for line in lines:
-        if(len(line)>=30):
-            total_log_requests+=1
-        return total_log_requests
-print('The requests made total: ', total_log_requests) 
+    total_log_requests=int()
+        lines = file.readlines()
+        for line in lines:
+            if(len(line)>=30):
+                total_log_requests+=1
+            return total_log_requests
+    print('The requests made total: ', total_log_requests) 
 
 #start looking for log requests made in the last year
 def main2():
-past_year_requests=int()
-    lines = file.readlines()
-    import datetime
-    import re
-    now = datetime.datetime.now
-    earlier = datetime.datetime(2019, 9, 17)
-    for line in lines:
-        data = lines.split()
-        date = data[3][1::].split(':')
-        if re.search(earlier>date<=now, line):
-              past_year_requests+=1
-        return past_year_requests
-
-#print the output results
-print('The requests made last year: ', past_year_requests)
+    past_year_requests=int()
+        lines = file.readlines()
+        import datetime
+        import re
+        now = datetime.datetime.now
+        earlier = datetime.datetime(2019, 9, 17)
+        for line in lines:
+            data = lines.split()
+            date = data[3][1::].split(':')
+            if re.search(earlier>date<=now, line):
+                  past_year_requests+=1
+            return past_year_requests
+    print('The requests made last year: ', past_year_requests)
 
 
 
