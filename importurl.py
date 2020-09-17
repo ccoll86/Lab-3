6184b45fd5030aa92c77104c09168bdaacfccab5
ccoll86 <70548702+ccoll86@users.noreply.github.com>
2020-09-17 13:33:39 -0500

    Update importurl.py

--- importurl.py
+++ importurl.py
@@ -16,20 +16,21 @@ file = open("aws.log", "r")
 lines = file.readlines()
 for lines in file:
     if(len(lines)>=56):
-        result1["total_log_requests: "]+=1
-    print(result1)    
+        result1["total_log_requests: "]+=1   
 
 #start looking for log requests made in the last year
 lines = file.readlines
 import datetime 
-x = datetime.datetime.now
-y = datetime.datetime(2019, 9, 17)
+now = datetime.datetime.now
+earlier = datetime.datetime(2019, 9, 17)
 
 for lines in file:
     data = lines.split()
     date = data[3][1::].split(':')
-    if(y>date<x):
+    if(earlier>date<now):
         result2["past_year_requests: "]+=1
-    print(result2)
+ 
+print(result1)    
+print(result2)
 
 
