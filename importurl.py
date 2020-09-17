801647a02b601d38c6d19ee486725fb2f8648795
ccoll86 <70548702+ccoll86@users.noreply.github.com>
2020-09-17 16:03:51 -0500

    Update importurl.py

--- importurl.py
+++ importurl.py
@@ -6,8 +6,8 @@ headers=urlretrieve(URL_PATH, Local_Copy)
 
 
 #reading file to look for 2 patterns
-total_log_requests=int()
-past_year_requests=int()
+total_log_requests={int()}
+past_year_requests={int()}
 
 file = open("aws.log", "r")
 
