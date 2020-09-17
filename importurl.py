2390cc5824f5fec5cb6fae836cbc2016b0ab67fb
ccoll86 <70548702+ccoll86@users.noreply.github.com>
2020-09-17 13:05:38 -0500

    Add python code

---
+++ importurl.py
@@ -0,0 +1,35 @@
+#retrieving log fles across network and creating new local location for the data
+import urllib.request 
+urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "aws.log")
+
+#reading file to look for 2 patterns
+result1={
+    "total_log_requests":0
+}
+result2={
+    "past_year_requests":{}
+}
+
+file = open("aws.log", "r")
+
+#start looking for total log requests
+lines = file.readlines()
+for lines in file:
+    if(len(lines)>=56):
+        result1["total_log_requests: "]+=1
+    print(result1)    
+
+#start looking for log requests made in the last year
+lines = file.readlines
+import datetime 
+x = datetime.datetime.now
+y = datetime.datetime(2019, 9, 17)
+
+for lines in file:
+    data = lines.split()
+    date = data[3][1::].split(':')
+    if(y>date<x):
+        result2["past_year_requests: "]+=1
+    print(result2)
+
+
