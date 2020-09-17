2a9139b07fa099e0f263d3464a850d289d104a9f
ccoll86 <70548702+ccoll86@users.noreply.github.com>
2020-09-17 13:56:23 -0500

    Update importurl.py

--- importurl.py
+++ importurl.py
@@ -3,8 +3,10 @@ import urllib.request
 urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "aws.log")
 
 #reading file to look for 2 patterns
-result1="total_log_requests: "
-result2="past_year_requests: "
+result1="total_log_requests: {}"
+result2="past_year_requests: {}"
+total_log_requests=0
+past_year_requests=0
 
 file = open("aws.log", "r")
 
@@ -12,7 +14,7 @@ file = open("aws.log", "r")
 lines = file.read()
 for lines in file:
     if(len(lines)>=56):
-        result1["total_log_requests: "]+=1   
+        result1["total_log_requests: "+=1]   
 
 #start looking for log requests made in the last year
 lines = file.read
@@ -24,9 +26,9 @@ for lines in file:
     data = lines.split()
     date = data[3][1::].split(':')
     if(earlier>date<=now):
-        result2["past_year_requests: "]+=1
+        result2["past_year_requests: "+=1]
  
-print(result1)    
-print(result2)
+print("This is how many log requests have been made: ", result1)    
+print("This is how many log requests have been made in the past year: ", result2)
 
 
