4098a846afead8115ea38721f67289a9b476a5d4
ccoll86 <70548702+ccoll86@users.noreply.github.com>
2020-09-18 11:13:48 -0500

    Update importurl.py

--- importurl.py
+++ importurl.py
@@ -24,31 +24,27 @@ if not os.path.isfile(Local_copy):
       
 #start looking for log requests made in the last year
 def yearly_logs():
-    
 #Adam's regex   
     pattern = r'(.*?) - (.*) \[(.*?)\] \"(.*?) (.*?)\"? (.+?) (.+) (.+)'
-
     lines = open(Local_copy, 'r').readlines()
-
+    import datetime
+    b=0
+    x = datetime.datetime.now
+    y = datetime.datetime(2019, 9, 18)
+    data = lines.split()
+    date = data[3][1::].split(':')
 #finding past year logs
     for line in lines:
             match = re.match(pattern, line)
-
             if not match:
                 continue
 
-import datetime
-b=0
-x = datetime.datetime.now
-y = datetime.datetime(2019, 9, 18)
-data = lines.split()
-date = data[3][1::].split(':')
-with open (Local_copy) as y:
-     if (y<date>=x):
-        for b, l in enumerate (y):
-             pass
-return b+1
-    
+    with open (Local_copy) as y:
+         if (y<date>=x):
+            for b, l in enumerate (y):
+                 pass
+    return b+1
+
 
 print('This is how many log requests have been made total: ', i)
 print('This is how many log requests have been made in the past year: ',b)
