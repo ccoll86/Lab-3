c8ed6d742a339f124e6d97f7e7a436193f51f0de
ccoll86 <70548702+ccoll86@users.noreply.github.com>
2020-09-18 15:14:31 -0500

    Update AWSlog.py

--- AWSlog.py
+++ AWSlog.py
@@ -1,15 +1,16 @@
-#import URL and creat local copy of log
+#import URL and create local copy of log
 import urllib.request 
 urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "aws.log")
 file = open("aws.log", "r")
 URL = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 Local_copy = 'aws.log'
 import os
+from datetime import datetime
 
 #defining variables and finding the information in local copy of log
 total_requests = 0
 last_year_requests = 0
-final_line = ""
+
 for line in file:
       total_requests += 1
       if line.find("1994")!= -1:
@@ -17,5 +18,6 @@ for line in file:
 file.close()
 
 #print final results found in log
-print("This is how many log requests were created ONLY last year: ", last_year_requests)
-print("This is how many TOTAL log requests were created in the file: ", total_requests)
+print("This is how many requests in the log file were created ONLY within the last year (1995): ", last_year_requests)
+
+print("This is how many TOTAL requests were created in the entire log file: ", total_requests)
