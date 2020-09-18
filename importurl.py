a8deeb86ef131310a878360ede1bc32a06feb828
ccoll86 <70548702+ccoll86@users.noreply.github.com>
2020-09-18 11:01:36 -0500

    Update importurl.py

--- importurl.py
+++ importurl.py
@@ -2,34 +2,54 @@
 import urllib.request 
 urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "aws.log")
 file = open("aws.log", "r")
-
+URL = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+Local_copy = 'aws.log'
+import os
+import re
 
 #start looking for total log requests
-total_log_requests=int()
-past_year_requests=int()
+i=0
 
-def main1():
+#finding length of lines
+def log_len(file):
     lines = file.readlines()
-    for line in lines:
-        if(len(line)>=30):
-             total_log_requests+=1
-        return total_log_requests
-print('The requests made total: ', total_log_requests) 
-
+    with open (Local_copy) as y:
+        if (len(Local_copy)>=30):
+        for i, l in enumerate (y)
+            pass
+        return i+1
+        
+    if not os.path.isfile(Local_copy):
+    urlretrieve(URL, Local_copy)   
+    
 #start looking for log requests made in the last year
-def main2():
-    lines = file.readlines()
-    import datetime
-    import re
-    now = datetime.datetime.now
-    earlier = datetime.datetime(2019, 9, 17)
-    for line in lines:
-         data = lines.split()
-         date = data[3][1::].split(':')
-         if re.search(earlier>date<=now, line):
-                  past_year_requests+=1
-         return past_year_requests
-print('The requests made last year: ', past_year_requests)
+def yearly_logs():
+    
+#Adam's regex   
+pattern = r'(.*?) - (.*) \[(.*?)\] \"(.*?) (.*?)\"? (.+?) (.+) (.+)'
 
+lines = open(Local_copy, 'r').readlines()
 
+#finding past year logs
+for line in lines:
+    match = re.match(pattern, line)
+
+    if not match:
+        continue
+
+  import datetime
+b=0
+x = datetime.datetime.now
+y = datetime.datetime(2019, 9, 18)
+data = lines.split()
+date = data[3][1::].split(':')
+with open (Local_copy) as y:
+        if (y<date>=x):
+        for b, l in enumerate (y)
+            pass
+        return b+1
+    
+
+print('This is how many log requests have been made total: ', i)
+print('This is how many log requests have been made in the past year: ',b)
 
